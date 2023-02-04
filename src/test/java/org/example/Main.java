d55d0e51ac64a986976da0c3198297b7c542d1c5
Oleg_Khlystov <oakhlystov@gmail.com>
2023-02-03 21:31:10 -0600

    deleted comments

--- src/test/java/org/example/Main.java
+++ src/test/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 
 import org.example.chrome.tests.ChromeTests;
-//import org.example.chrome.tests.helpers.TabsController;
 
 public class Main {
     public static void main(String[] args) {
         ChromeTests ct = new ChromeTests();
-//        TabsController tc = new TabsController();
         ct.startTests();
-//        tc.closeAllTabs();
     }
 }
